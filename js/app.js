17928dbd50173fa5b239994fb71fd2abc18d7883
IvonReD <ivonne.reyes.dc@gmail.com>
2017-12-05 12:07:43 -0600

    Actualizando Readme, css y js

--- js/app.js
+++ js/app.js
@@ -14,3 +14,42 @@ function begin(){
     boxForm.classList.toggle("none");
     text.focus();
   }
+
+  /*Versión 0.0.2:Al dar click en el botón de "Guardar", se mostrará un nuevo cuadro donde estará el nombre de la lista agregada. Mostrar un texto de "Añadir una tarea" dentro de la lista.*/
+  boton.addEventListener("click", saveEvent);
+  function saveEvent(event){
+    event.preventDefault();
+    if(text.value){
+      boxForm.classList.toggle("none");
+      var title = text.value;
+      var boxTitle = document.createElement("div");
+      boxTitle.textContent = title;
+      boxTitle.classList.add("bt");
+      container.appendChild(boxTitle);
+
+      var linkTask = document.createElement("input");
+      linkTask.setAttribute("placeholder", "Añadir una tarea...");
+      linkTask.classList.add("linkTask");
+      container.appendChild(linkTask);
+    }
+
+    /*Versión 0.0.3:Al dar click en "Añadir una tarea", deberá mostrar un formulario con un textarea y un botón que diga "Añadir".*/
+      linkTask.addEventListener("click", addTareas);
+      function addTareas(event){
+      linkTask.classList.toggle("none");
+
+      var form = document.createElement("form");
+      container.appendChild(form);
+
+      var task = document.createElement("textarea");
+      task.classList.add("newtask");
+      form.appendChild(task);
+
+      var taskBtn = document.createElement("button");
+      taskBtn.setAttribute("id", "btn");
+      taskBtn.textContent = "Añadir";
+
+
+      taskBtn.classList.add("button");
+      taskBtn.classList.add("second");
+      form.appendChild(taskBtn);
